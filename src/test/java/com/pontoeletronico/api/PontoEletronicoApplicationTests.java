0a206ba6a456e5f67e9b668ab50a28fae14c85c1
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-04-15 21:06:25 -0300

    ajuste sonar

--- src/test/java/com/pontoeletronico/api/PontoEletronicoApplicationTests.java
+++ src/test/java/com/pontoeletronico/api/PontoEletronicoApplicationTests.java
@@ -2,6 +2,8 @@ package com.pontoeletronico.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,8 +13,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class PontoEletronicoApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(PontoEletronicoApplicationTests.class);
+	
 	@Test
 	public void contextLoads() {
+		log.info("Teste");
 	}
 
 }
