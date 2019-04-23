18bb93a6b6a3a9b821ed0a8cb858075d08082c75
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-04-23 19:16:04 -0300

    Adicionado filtros para autenticacao

---
+++ src/main/java/com/pontoeletronico/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.pontoeletronico.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pontoeletronico.api.entities.Funcionario;
+import com.pontoeletronico.api.security.JwtUserFactory;
+import com.pontoeletronico.api.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
