1986947b8a3cdcfcb1e6905efda4e9dd47c35f2c
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-04-25 19:47:52 -0300

    Adicionado utilização de cache

--- src/main/java/com/pontoeletronico/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/pontoeletronico/api/services/impl/LancamentoServiceImpl.java
@@ -28,13 +28,13 @@ public class LancamentoServiceImpl implements LancamentoService {
 		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 	
-	@Cacheable("lancamentoPorId")
+	@Cacheable("lancamentoPorId")//armazena no cache. Mesmo nome da configuracao no xml
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento pelo ID {}", id);
 		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
 	}
 	
-	@CachePut("lancamentoPorId")
+	@CachePut("lancamentoPorId")//atualiza o cache
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo o lançamento: {}", lancamento);
 		return this.lancamentoRepository.save(lancamento);
