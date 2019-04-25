2927ea6d0b14fe1a1da5a3d2e605be48739629c7
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-04-25 19:47:40 -0300

    habilitando cache na API

--- src/main/java/com/pontoeletronico/api/PontoEletronicoApplication.java
+++ src/main/java/com/pontoeletronico/api/PontoEletronicoApplication.java
@@ -2,8 +2,10 @@ package com.pontoeletronico.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching//habilita utilização de cache
 public class PontoEletronicoApplication {
 
 	public static void main(String[] args) {
